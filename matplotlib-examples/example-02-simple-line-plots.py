5cf8407e9390a06e18b620e372a4475b7a5aae66
ax-va <vasiliev.informatik@t-online.de>
2023-07-25 21:33:55 +0200

    update

--- matplotlib-examples/example-02-simple-line-plots.py
+++ matplotlib-examples/example-02-simple-line-plots.py
@@ -1,5 +1,38 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+
+matplotlib.style.available
+# ['Solarize_Light2',
+#  '_classic_test_patch',
+#  '_mpl-gallery',
+#  '_mpl-gallery-nogrid',
+#  'bmh',
+#  'classic',
+#  'dark_background',
+#  'fast',
+#  'fivethirtyeight',
+#  'ggplot',
+#  'grayscale',
+#  'seaborn-v0_8',
+#  'seaborn-v0_8-bright',
+#  'seaborn-v0_8-colorblind',
+#  'seaborn-v0_8-dark',
+#  'seaborn-v0_8-dark-palette',
+#  'seaborn-v0_8-darkgrid',
+#  'seaborn-v0_8-deep',
+#  'seaborn-v0_8-muted',
+#  'seaborn-v0_8-notebook',
+#  'seaborn-v0_8-paper',
+#  'seaborn-v0_8-pastel',
+#  'seaborn-v0_8-poster',
+#  'seaborn-v0_8-talk',
+#  'seaborn-v0_8-ticks',
+#  'seaborn-v0_8-white',
+#  'seaborn-v0_8-whitegrid',
+#  'tableau-colorblind10']
+
+plt.style.use('seaborn-v0_8-whitegrid')
 
 x = np.linspace(0, 10, 1000)
 
@@ -7,18 +40,18 @@ fig = plt.figure()
 ax = plt.axes()
 
 ax.plot(x, np.sin(x))
-plt.savefig('../matplotlib-examples-figures/simple-line-plots--sin-1.svg')
+plt.savefig('../matplotlib-examples-figures/simple-line-plots-01--sin-1.svg')
 plt.close()
 
 # alternatively
 plt.plot(x, np.sin(x))
-plt.savefig('../matplotlib-examples-figures/simple-line-plots--sin-2.svg')
+plt.savefig('../matplotlib-examples-figures/simple-line-plots-02--sin-2.svg')
 plt.close()
 
 # Create a single figure with multiple lines
 plt.plot(x, np.sin(x))
 plt.plot(x, np.cos(x))
-plt.savefig('../matplotlib-examples-figures/simple-line-plots--multiple-lines.svg')
+plt.savefig('../matplotlib-examples-figures/simple-line-plots-03--multiple-lines.svg')
 plt.close()
 
 # colors and linestyles
@@ -37,7 +70,7 @@ plt.plot(x, np.sin(x - 3), color='#FFDD44')
 plt.plot(x, np.sin(x - 4), color=(1.0,0.2,0.3))
 # HTML color names supported
 plt.plot(x, np.sin(x - 5), color='chartreuse')
-plt.savefig('../matplotlib-examples-figures/simple-line-plots--colored-lines.svg')
+plt.savefig('../matplotlib-examples-figures/simple-line-plots-04--colored-lines.svg')
 plt.close()
 
 # linestyles
@@ -46,7 +79,7 @@ plt.plot(x, x + 0, linestyle='solid')
 plt.plot(x, x + 1, linestyle='dashed')
 plt.plot(x, x + 2, linestyle='dashdot')
 plt.plot(x, x + 3, linestyle='dotted')
-plt.savefig('../matplotlib-examples-figures/simple-line-plots--linestyles-1.svg')
+plt.savefig('../matplotlib-examples-figures/simple-line-plots-05--linestyles-1.svg')
 plt.close()
 
 # For short, you can use the following codes:
@@ -54,7 +87,7 @@ plt.plot(x, x + 4, linestyle='-')  # solid
 plt.plot(x, x + 5, linestyle='--')  # dashed
 plt.plot(x, x + 6, linestyle='-.')  # dashdot
 plt.plot(x, x + 7, linestyle=':')  # dotted
-plt.savefig('../matplotlib-examples-figures/simple-line-plots--linestyles-2.svg')
+plt.savefig('../matplotlib-examples-figures/simple-line-plots-06--linestyles-2.svg')
 plt.close()
 
 # combined the linestyle and color codes
@@ -63,7 +96,7 @@ plt.plot(x, x + 8, '-g')  # solid green
 plt.plot(x, x + 9, '--c')  # dashed cyan
 plt.plot(x, x + 10, '-.k')  # dashdot black
 plt.plot(x, x + 11, ':r')  # dotted red
-plt.savefig('../matplotlib-examples-figures/simple-line-plots--colored-linestyles.svg')
+plt.savefig('../matplotlib-examples-figures/simple-line-plots-07--colored-linestyles.svg')
 plt.close()
 
 # axes limits
@@ -71,23 +104,23 @@ plt.close()
 plt.plot(x, np.sin(x))
 plt.xlim(-1, 11)
 plt.ylim(-1.5, 1.5)
-plt.savefig('../matplotlib-examples-figures/simple-line-plots--axes-limits.svg')
+plt.savefig('../matplotlib-examples-figures/simple-line-plots-08--axes-limits.svg')
 plt.close()
 
 plt.plot(x, np.sin(x))
 plt.xlim(10, 0)
 plt.ylim(1.2, -1.2)
-plt.savefig('../matplotlib-examples-figures/simple-line-plots--reversed-axis.svg')
+plt.savefig('../matplotlib-examples-figures/simple-line-plots-09--reversed-axis.svg')
 plt.close()
 
 plt.plot(x, np.sin(x))
 plt.axis('tight')
-plt.savefig('../matplotlib-examples-figures/simple-line-plots--tight-axis.svg')
+plt.savefig('../matplotlib-examples-figures/simple-line-plots-10--tight-axis.svg')
 plt.close()
 
 plt.plot(x, np.sin(x))
 plt.axis('equal')  # equal axis ratio
-plt.savefig('../matplotlib-examples-figures/simple-line-plots--equal-axis-ratio.svg')
+plt.savefig('../matplotlib-examples-figures/simple-line-plots-11--equal-axis-ratio.svg')
 plt.close()
 
 # Other axis options: 'on', 'off', 'square', 'image', and more
@@ -98,14 +131,14 @@ plt.plot(x, np.sin(x))
 plt.title("A Sine Curve")
 plt.xlabel("$x$")
 plt.ylabel("$\sin(x)$")
-plt.savefig('../matplotlib-examples-figures/simple-line-plots--labels.svg')
+plt.savefig('../matplotlib-examples-figures/simple-line-plots-12--labels.svg')
 plt.close()
 
 plt.plot(x, np.sin(x), '-g', label='$\sin(x)$')
 plt.plot(x, np.cos(x), ':b', label='$\cos(x)$')
 plt.axis('equal')
 plt.legend()
-plt.savefig('../matplotlib-examples-figures/simple-line-plots--legend.svg')
+plt.savefig('../matplotlib-examples-figures/simple-line-plots-13--legend.svg')
 plt.close()
 
 # Matplotlib gotchas
